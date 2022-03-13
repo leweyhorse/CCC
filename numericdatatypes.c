3b58638d404dfba7635b3de541a5996b2b7a3550
leweyhorse <44213730+leweyhorse@users.noreply.github.com>
2022-03-13 12:21:12 -0600

    Add files via upload

---
+++ numericdatatypes.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	//25000 
+	int dogs = 2.5e4;
+
+	//floating point
+	//precision -- how much data the variable can hold
+	// floats are stored in scientific notation
+	/*
+	printf("int %i double %f float %f .\n", 1, 1.1111, 1.1111F);
+	printf("int %i double %f float %f .\n", 1, 1111.1111, 1111.1111F);
+	*/
+	printf("dogs = %i.\n", dogs);
+
+	return 0;
+}
